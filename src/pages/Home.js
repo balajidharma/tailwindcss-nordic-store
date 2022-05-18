8dd5c76f3aeb877d64719970b9402453e9d8e03b
Balaji D <balajidharma@2610158.no-reply.drupal.org>
2022-05-17 23:07:45 -0400

    Chapter 2: Consume a RESTful API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,102 +1,63 @@
 import React from 'react';
 
-const products = [
-  {
-    id: 1,
-    name: 'Product Name',
-    href: '#',
-    price: '$9.99',
-    imageSrc: 'https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
-  },
-  {
-    id: 2,
-    name: 'Product Name',
-    href: '#',
-    price: '$10.99',
-    imageSrc: 'https://images.unsplash.com/photo-1508423134147-addf71308178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
-  },
-  {
-    id: 3,
-    name: 'Product Name',
-    href: '#',
-    price: '$12.99',
-    imageSrc: 'https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
-  },
-  {
-    id: 4,
-    name: 'Product Name',
-    href: '#',
-    price: '$9.99',
-    imageSrc: 'https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
-  },
-  {
-    id: 5,
-    name: 'Product Name',
-    href: '#',
-    price: '$6.99',
-    imageSrc: 'https://images.unsplash.com/photo-1467949576168-6ce8e2df4e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
-  },
-  {
-    id: 6,
-    name: 'Product Name',
-    href: '#',
-    price: '$10.99',
-    imageSrc: 'https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
-  },
-  {
-    id: 7,
-    name: 'Product Name',
-    href: '#',
-    price: '$22.99',
-    imageSrc: 'https://images.unsplash.com/photo-1550837368-6594235de85c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
-  },
-  {
-    id: 8,
-    name: 'Product Name',
-    href: '#',
-    price: '$19.99',
-    imageSrc: 'https://images.unsplash.com/photo-1551431009-a802eeec77b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=400&q=80',
-  },
-]
-
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/data/products.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            products: result.products
+          });
+        }
+      )
+  }
+
   render() {
+    const { isLoaded, products } = this.state;
     return (
       <div>
-
         <section className="bg-white py-8">
-
           <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-
             <nav id="store" className="w-full z-30 top-0 px-6 py-1">
               <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
-
                 <a className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
                   Store
                 </a>
-
                 <div className="flex items-center" id="store-nav-content">
-
                   <a className="pl-3 inline-block no-underline hover:text-black" href="#">
                     <svg className="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                       <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
                     </svg>
                   </a>
-
                   <a className="pl-3 inline-block no-underline hover:text-black" href="#">
                     <svg className="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                       <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
                     </svg>
                   </a>
-
                 </div>
               </div>
             </nav>
-
+            {!isLoaded ? (
+              <div class="w-full">
+                <div class="flex items-center justify-center">
+                  Loading...
+                </div>
+              </div>
+            ) : (
             <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
               {products.map((product) => (
                 <a key={product.id} href={product.href}>
-                  <img className="hover:grow hover:shadow-lg" src={product.imageSrc} />
+                  <img className="hover:grow hover:shadow-lg" src={product.thumbnail} />
                   <div className="pt-3 flex items-center justify-between">
                     <p className="">{product.name}</p>
                     <svg className="h-6 w-6 fill-current text-gray-500 hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -107,11 +68,9 @@ class Home extends React.Component {
                 </a>
               ))}
             </div>
-
+            )}
           </div>
-
         </section>
-
       </div>
     );
   }
