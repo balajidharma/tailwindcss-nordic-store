309d8b11e2e5448edefe9a45a32e25b57bb96b0a
balajidharma <balajidharma@hotmail.com>
2022-12-02 10:38:58 -0500

    Added about us page

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
+import About from './pages/About';
 
 ReactDOM.render(
   <BrowserRouter>
     <Layout>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
       </Routes>
     </Layout>
   </BrowserRouter>,
---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+    <div>
+      <section class="bg-white py-8">
+        <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+          <h2 class="w-full uppercase tracking-wide font-bold text-gray-800 text-xl">About Us</h2>
+          <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus commodo nec id erat.
+              Suspendisse consectetur dapibus velit ut lacinia. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.</p>
+          </div>
+        </div>
+      </section>
+    </div>
+    )
+  }
+}
+
+export default About;
